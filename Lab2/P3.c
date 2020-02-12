5514ed7c02c739b8c114b67bdad256f6b189b447
Trantin84 <trantin84@gmail.com>
2020-02-12 10:00:23 +0700

    Create P3.c

---
+++ Lab2/P3.c
@@ -0,0 +1,17 @@
+// P3.c
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{ pid_t pid;
+ pid = fork();
+ if(pid < 0) printf(“fork error! No child created\n”);
+ else if(pid == 0) {
+ printf(“Hello from Child!\n”);
+ printf(“Child ID is %d!\n”, getpid());
+ }
+ else {
+ printf(“Hello from Parent!\n”
+ printf(“Parent ID is %d!\n”, getpid());
+ }
+ return 0;
+}
