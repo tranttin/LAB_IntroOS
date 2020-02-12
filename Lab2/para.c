9cc13478f811b61498f0159616814e6d8eb014dc
Trantin84 <trantin84@gmail.com>
2020-02-12 09:52:36 +0700

    Create para.c

---
+++ Lab2/para.c
@@ -0,0 +1,9 @@
+// para.c
+#include <stdio.h>
+int main(int argc, char ** argv) {
+ printf("Number of arguments %i n", argc);
+ int i=0;
+ for(i=0; i<argc; i++)
+ prinf(“Argument %s\t”, argv[i];
+ return 0;
+}
