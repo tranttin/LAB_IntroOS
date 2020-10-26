c346e80476cdba121d406746000cad6d5b70abdf
Trantin84 <trantin84@gmail.com>
2020-10-26 07:29:01 +0700

    Update Pseudocode.c

--- LAB 9/Contiguous Memory Allocation/Pseudocode.c
+++ LAB 9/Contiguous Memory Allocation/Pseudocode.c
@@ -1,16 +1,24 @@
-int MAX (kích thước bộ nhớ)
+
+bool mem[2000000000]; //maximum of int
 int option=1;
 
 void function1()
-void function1()
-void function1()
-void function1()
-
+      hỏi kích thước tiến trình cần cấp bao nhiêu? size.
+      for i = 0 --> MAX-size
+         if (mem[i] = false) then "mem[i] đến mem[i+size-1]" có trống hay không/có đều là false hay không. Nếu có thì cấp phát: gán toàn bộ mem[i] đoạn đó là true.
+      Sau khi vòng lặp kết thúc vẫn không cấp được thì trả lời là không cấp được.
+               
+void function2()
+void function3()
+void function4()
+               
+      for i = 0 --> MAX
+         if (mem[i] = false) thì dò tiếp mem i++ cho đến khi gặp mem[j] là true. Print "Memory free from i to j-1" rồi tiếp tục dò.
 
 
 main(    )
 MAX = atoi(argv[1]);  // truyền kích thước vào khi gọi chạy 
-bool mem[MAX];
+
 for(0 --> MAX - 1)   mem[i] = false //ban đầu bộ nhớ coi như trống hết.
 
 
@@ -18,20 +26,22 @@ while(option != 5)
 {     print "Chọn option   1-Cap phat   2-Thu hoi   3-Gom cum   4-Thong ke"
       scanf   option
       switch (option) of
-      1
+      1  tạo tiểu trình A chạy hàm fun 1
       
       
-      2
-      
-      
-      
-      3
+      2  tạo tiểu trình B chạy hàm fun 2
       
       
       
-      4
+      3  tạo tiểu trình C chạy hàm fun 3
+      
+      
+      
+      4  tạo tiểu trình D chạy hàm fun 4
 
-      5: return.
+            
+            
+      5: return. //thoát
       
 
 
