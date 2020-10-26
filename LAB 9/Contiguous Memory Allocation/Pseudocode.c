9692566d871335316fbdb36408e37950c3861ffb
Trantin84 <trantin84@gmail.com>
2020-10-26 07:39:17 +0700

    Update Pseudocode.c

--- LAB 9/Contiguous Memory Allocation/Pseudocode.c
+++ LAB 9/Contiguous Memory Allocation/Pseudocode.c
@@ -5,17 +5,25 @@ int option=1;
 void function1()
       hỏi kích thước tiến trình cần cấp bao nhiêu? size.
       for i = 0 --> MAX-size
-         if (mem[i] = false) then "mem[i] đến mem[i+size-1]" có trống hay không/có đều là false hay không. Nếu có thì cấp phát: gán toàn bộ mem[i] đoạn đó là true.
+         if (mem[i] = false) then "mem[i] đến mem[i+size-1]" có trống hay không/có đều là false hay không. Nếu có thì cấp phát: gán toàn bộ mem[i] đoạn đó là true. Đồng thời ghi nhận lại "Tiến trình X đã được cấp mem từ i đến ..."
+               
       Sau khi vòng lặp kết thúc vẫn không cấp được thì trả lời là không cấp được.
                
+               
 void function2()
+      kiểm tra xem tiến trình X được cấp trong đoạn bộ nhớ i đến j nào, 
+      for (k=i to j) mem[k]= false;  //coi như vùng nhớ này trống trở lại
+     xóa thông tin "Tiến trình X đã được cấp mem từ i đến ..." ra khỏi danh mục (vì X coi như đã kết thúc)
+
 void function3()
 void function4()
                
-      for i = 0 --> MAX
+     Cách 1:  for i = 0 --> MAX
          if (mem[i] = false) thì dò tiếp mem i++ cho đến khi gặp mem[j] là true. Print "Memory free from i to j-1" rồi tiếp tục dò.
 
-
+     Cách 2: In ra danh mục "Tiến trình X đã được cấp mem từ i đến ..."
+           
+           
 main(    )
 MAX = atoi(argv[1]);  // truyền kích thước vào khi gọi chạy 
 
