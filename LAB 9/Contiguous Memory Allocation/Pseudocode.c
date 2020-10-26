516a886aa0f2195f805ecc685d608d505b3757cc
Trantin84 <trantin84@gmail.com>
2020-10-26 07:22:33 +0700

    Create Pseudocode.c

---
+++ LAB 9/Contiguous Memory Allocation/Pseudocode.c
@@ -0,0 +1,42 @@
+int MAX (kích thước bộ nhớ)
+int option=1;
+
+void function1()
+void function1()
+void function1()
+void function1()
+
+
+
+main(    )
+MAX = atoi(argv[1]);  // truyền kích thước vào khi gọi chạy 
+bool mem[MAX];
+for(0 --> MAX - 1)   mem[i] = false //ban đầu bộ nhớ coi như trống hết.
+
+
+while(option != 5)
+{     print "Chọn option   1-Cap phat   2-Thu hoi   3-Gom cum   4-Thong ke"
+      scanf   option
+      switch (option) of
+      1
+      
+      
+      2
+      
+      
+      
+      3
+      
+      
+      
+      4
+
+      5: return.
+      
+
+
+
+
+
+
+}
