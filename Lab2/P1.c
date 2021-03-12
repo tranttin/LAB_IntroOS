35a33c8cfc69a5e4d362ab103232460b7f76f1a7
Tran T. Tin <trantin84@gmail.com>
2021-03-12 10:50:44 +0700

    Create P1.c

--- Lab2/P1.c
+++ Lab2/P1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 int main() {
-     printf(“Current process ID: %d\n”, getpid());
-     printf(“Current process ID: %d\n”, getpid())
+     printf("Current process ID: %d\n", getpid());
+     printf("Current process ID: %d\n", getpid());
      return 0;
 }
