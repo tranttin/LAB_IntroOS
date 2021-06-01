8ff1554888e01d9c0e0fa8012fcef0048f2fa115
Tran T. Tin <trantin84@gmail.com>
2021-06-01 21:37:10 +0700

    Update Fig3_25_Consumer_AnonymousPipe_WIN.c

--- CHAPTER 3/Fig3_25_Consumer_AnonymousPipe_WIN.c
+++ CHAPTER 3/Fig3_25_Consumer_AnonymousPipe_WIN.c
@@ -2,15 +2,15 @@
 
 #include <windows.h>
 
-#define BUFFER SIZE 25
+#define BUFFER_SIZE 25
 int main(VOID) {
   HANDLE Readhandle;
-  CHAR buffer[BUFFER SIZE];
+  CHAR buffer[BUFFER_SIZE];
   DWORD read;
   /* get the read handle of the pipe */
-  ReadHandle = GetStdHandle(STD INPUT HANDLE);
+  ReadHandle = GetStdHandle(STD_INPUT_HANDLE);
   /* the child reads from the pipe */
-  if (ReadFile(ReadHandle, buffer, BUFFER SIZE, & read, NULL))
+  if (ReadFile(ReadHandle, buffer, BUFFER_SIZE, &read, NULL))
     printf("child read %s", buffer);
   else
     fprintf(stderr, "Error reading from pipe");
