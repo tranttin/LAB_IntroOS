eaac4de09970413f81cffe7bbfbed3841a4cc984
Tran T. Tin <trantin84@gmail.com>
2021-06-04 16:03:21 +0700

    Delete Lab3 directory

--- Lab3/vd1_1.c
+++
@@ -1,8 +0,0 @@
-// P1.c
-#include <stdio.h>
-#include <unistd.h>
-int main() {
-    printf("Current process ID: %d\n", getpid());
-    printf("Parent process ID: %d\n", getppid());
-    return 0;
-}
