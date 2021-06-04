4aa225b26813e3e89d37b8e242a87217fed02e5d
Tran T. Tin <trantin84@gmail.com>
2021-06-04 16:33:38 +0700

    Update and rename CHAPTER 3/Fig3_21_OrdinaryPipe_UNIX.c to CHAPTER_3/Fig3_21_OrdinaryPipe_UNIX.c

--- CHAPTER_3/Fig3_21_OrdinaryPipe_UNIX.c
+++ CHAPTER_3/Fig3_21_OrdinaryPipe_UNIX.c
@@ -1,5 +1,6 @@
-#include <sys/types.h>
-
+// 2021 June 2
+// Author: Abraham Silberschatz  in book Operating System Concepts 10th Edition p.170
+// Demo using how to speed up sum of sequence integer.#include <sys/types.h>
 #include <stdio.h>
 
 #include <string.h>
