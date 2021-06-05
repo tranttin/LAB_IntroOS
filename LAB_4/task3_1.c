14fe138eca16e496ecfb56bcd54d2534122d64c1
Tran T. Tin <trantin84@gmail.com>
2021-06-05 20:24:01 +0700

    Update task3_1.c

--- LAB_4/task3_1.c
+++ LAB_4/task3_1.c
@@ -12,7 +12,6 @@
 #define M 10
 #define N 10
 #define MAX_INT 10000
-#define MAX_LENGTH 100
 int a[M][N];
 void * writer(void * param); /* threads call this function */
 int main(int argc, char * argv[]) {
@@ -20,15 +19,9 @@ int main(int argc, char * argv[]) {
   pthread_attr_t attr; /* set of thread attributes */
   /* set the default attributes of the thread */
   pthread_attr_init( & attr);
-
   /* Intializes random number generator */
   time_t t;
   srand((unsigned) time( & t));
-  char line[MAX_LENGTH];
-  char tokens[] = " \n";
-  char * key;
-  memset(line, 0, MAX_LENGTH);
-
   for (int i = 0; i < M; i++) {
     for (int j = 0; j < N; j++)
       a[i][j] = rand() % MAX_INT;
@@ -37,7 +30,6 @@ int main(int argc, char * argv[]) {
   pthread_create( & tid, & attr, writer, argv[1]);
   /* wait for the thread to exit */
   pthread_join(tid, NULL);
-
 }
 /* The thread will execute in this function */
 void * writer(void * param) {
