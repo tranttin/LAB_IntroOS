a50596b10db9e5b8774c19e3e177bcc600cac4f0
Tran T. Tin <trantin84@gmail.com>
2021-06-05 08:36:49 +0700

    Update task1_2.c

--- LAB_4/task1_2.c
+++ LAB_4/task1_2.c
@@ -6,44 +6,46 @@
 #include<stdio.h>
 
 #include<stdlib.h>
-/* gettimeofday */
-#include <sys/time.h>
-/* clock */
-#include <time.h>
+
+/* gettimeofday */ #include <sys/time.h>
+
+/* clock */ #include <time.h>
 
 #include <sys/sysinfo.h>
+
+/* pid_t */ #include <sys/types.h>
 
 int sum; /* this data is shared by the thread(s) */
 void * runner(void * param); /* threads call this function */
 int main(int argc, char * argv[]) {
-  pthreadt tid; /* the thread identifier */
-  pthreadattrt attr; /* set of thread attributes */
+  pthread_t tid; /* the thread identifier */
+  pthread_attr_t attr; /* set of thread attributes */
   /* set the default attributes of the thread */
-  pthreadattrinit( & attr);
+  pthread_attr_init( & attr);
   /* create the thread */
-  
-  time_t startwatch, endwatch;
-  struct timeval startCPU, endCPU;
-  
+
+  time_t startCPU, endCPU;
+  struct timeval startwatch, endwatch;
+
   /* ### start section to be measured ### */
-	/* clock() method */
-	startCPU = clock();
-	/* gettimeofday() method */
-	gettimeofday(&startwatch, NULL);
-  
-  pthreadcreate( & tid, & attr, runner, argv[1]);
+  /* clock() method */
+  startCPU = clock();
+  /* gettimeofday() method */
+  gettimeofday( & startwatch, NULL);
+
+  pthread_create( & tid, & attr, runner, argv[1]);
   /* wait for the thread to exit */
-  pthreadjoin(tid, NULL);
+  pthread_join(tid, NULL);
 
   endCPU = clock();
-	gettimeofday(&endwatch, NULL);
-	/* ### end of section to be measured ### */
+  gettimeofday( & endwatch, NULL);
+  /* ### end of section to be measured ### */
 
-  printf("This system has %d processors configured and %d processors available.\n",   get_nprocs_conf(), get_nprocs());
-  printf("\nClock() method: %ldms\n", (endCPU - startCPU) / (CLOCKS_PER_SEC/1000));
-	printf(" gettimeofday() method: %ldms\n", (endwatch.tv_sec - startwatch.tv_sec) *1000 + (endwatch.tv_usec - startwatch.tv_usec)/1000);
-  
-  printf("sum = %d∖n", sum);
+  printf("\nThis system has %d processors configured and %d processors available.", get_nprocs_conf(), get_nprocs());
+  printf("\nClock() method: %ldus\n", (endCPU - startCPU) / (CLOCKS_PER_SEC / 1000000));
+  printf("\nGettimeofday() method: %ldus", (endwatch.tv_sec - startwatch.tv_sec) * 1000000 + (endwatch.tv_usec - startwatch.tv_usec));
+
+  printf("\nSum = %d∖n", sum);
 }
 
 /* The thread will execute in this function */
@@ -52,5 +54,5 @@ void * runner(void * param) {
   sum = 0;
   for (i = 1; i <= upper; i++)
     sum += i;
-  pthreadexit(0);
+  pthread_exit(0);
 }
