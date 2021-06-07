c20e6b414ea6b512902859a8addf1408ba710bcf
Tran T. Tin <trantin84@gmail.com>
2021-06-07 21:36:51 +0700

    Update task4_1reader.c

--- LAB_5/task4_1reader.c
+++ LAB_5/task4_1reader.c
@@ -9,7 +9,7 @@
 
 #include <stdlib.h>
 
-#include "queue.h"
+#include "key.h"
 
 void report_and_exit(const char * msg) {
   perror(msg);
@@ -23,20 +23,13 @@ int main() {
   int qid = msgget(key, 0666 | IPC_CREAT); /* access if created already */
   if (qid < 0) report_and_exit("no access to queue...");
 
-  int types[] = {
-    3,
-    1,
-    2,
-    1,
-    3,
-    2
-  }; /* different than in sender */
+  int priority[MsgCount];
   int i;
   for (i = 0; i < MsgCount; i++) {
-    queuedMessage msg; /* defined in queue.h */
-    if (msgrcv(qid, & msg, sizeof(msg), types[i], MSG_NOERROR | IPC_NOWAIT) < 0)
+    queuedMessage msg; /* defined in key.h */
+    if (msgrcv(qid, & msg, sizeof(msg), &priority[i], MSG_NOERROR | IPC_NOWAIT) < 0)
       puts("msgrcv trouble...");
-    printf("%s received as type %i\n", msg.payload, (int) msg.type);
+    printf("%s (priority %i\n", msg.payload, (int) msg.priority);
   }
 
   /** remove the queue **/
