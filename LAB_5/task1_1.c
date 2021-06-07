e56b1cf4b25bc2121abf06e01e227a8bc6fdadaf
Tran T. Tin <trantin84@gmail.com>
2021-06-07 09:25:45 +0700

    Create task1_1.c

---
+++ LAB_5/task1_1.c
@@ -0,0 +1,24 @@
+// 2021 June 2
+// Author: 
+// Demo using system call fork to create a child process and modify a global varible
+#include<sys/types.h>
+
+#include<stdio.h>
+
+#include<unistd.h>
+
+int value = 5;
+int main() {
+  pid_t pid;
+  pid = fork();
+  if (pid == 0) {
+    /* child process */
+    value += 15;
+    return 0;
+  } else if (pid > 0) {
+    /* parent process */
+    wait(NULL);
+    printf("PARENT: value = %d", value); /* LINE A */
+    return 0;
+  }
+}
