c80e80702c7eca7915623075b938af4930e089e3
Tran T. Tin <trantin84@gmail.com>
2021-06-07 15:55:44 +0700

    Update task2_2child.c

--- LAB_5/task2_2child.c
+++ LAB_5/task2_2child.c
@@ -27,33 +27,27 @@ extern int errno;
 
 int main(int argc, char * argv[]) {
   char s1[PIPE_BUF], s2[PIPE_BUF], buf[PIPE_BUF];
-  int childpid, readfd, writefd;
-  if ((mknod(FIFO1, S_IFIFO | PM, 0) < 0) && (errno != EEXIST)) {
-    printf("Fail to create FIFO 1. Aborted.\n");
+  int readfd, writefd;
+  if ((mknod(FIFO2, S_IFIFO | PM, 0) < 0) && (errno != EEXIST)) {
+    printf("Fail to create FIFO 2. Aborted.\n");
     return -1;
   }
 
-  if ((writefd = open(FIFO1, 1)) < 0)
-    perror("Parent cannot open writeFIFO.\n");
-  if ((readfd = open(FIFO2, 0)) < 0)
+  if ((readfd = open(FIFO1, 0)) < 0)
     perror("Child cannot open readFIFO.\n");
+  if ((writefd = open(FIFO2, 1)) < 0)
+    perror("Child cannot open writeFIFO.\n");
   do {
+    read(readfd, s2, PIPE_BUF);
+    printf("Parent said: %s", s2);
     printf("(me): ");
     fgets(s1, PIPE_BUF, stdin);
     if (strcmp(s1, "byebye\n") == 0) break;
     write(writefd, s1, strlen(s1));
-    read(readfd, s2, PIPE_BUF);
-    printf("Child said: %s", s2);
 
-  }
-  while (1);
+  } while (1);
 
   close(readfd);
   close(writefd);
-  if (unlink(FIFO1) < 0)
-    perror("Cannot remove FIFO1.\n");
-  if (unlink(FIFO2) < 0)
-    perror("Cannot remove FIFO2.\n");
   return 1;
-
 }
