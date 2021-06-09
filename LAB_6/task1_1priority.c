19200df200490332b7fa657a56ce689b5b574b4e
Tran T. Tin <trantin84@gmail.com>
2021-06-09 10:59:16 +0700

    Update task1_1priority.c

--- LAB_6/task1_1priority.c
+++ LAB_6/task1_1priority.c
@@ -1,15 +1,17 @@
 // 2021 June 7
 // Author: Tran Trung Tin
 // Calculating value of PI by Monte Carlo method
-#include<pthread.h>
+#define _GNU_SOURCE#include<pthread.h>
 
 #include<stdio.h>
 
 #include<stdlib.h>
 
-/* gettimeofday */ #include <sys/time.h>
+/* gettimeofday */
+#include <sys/time.h>
 
-/* clock */ #include <time.h>
+/* clock */
+#include <time.h>
 
 #include <sys/sysinfo.h>
 
@@ -18,38 +20,67 @@
 #include <sys/syscall.h>
 
 #include <unistd.h>
- // maximum number of threads 
+
+
+#include <sched.h>
+
+#include <sys/wait.h>
+
+
+// maximum number of threads 
 #define MAX_THREAD 40
 int counter = 0; /* this data is shared by the thread(s) */
 void * runner(void * param); /* threads call this function */
 int main(int argc, char * argv[]) {
   pthread_t tid[MAX_THREAD]; /* the thread identifier */
-  pthread_attr_t attr; /* set of thread attributes */
-  /* set the default attributes of the thread */
-  pthread_attr_init( & attr);
-  /* create the thread */
 
   struct timeval startwatch, endwatch;
 
   int n_thread = atoi(argv[1]);
- 
-//  https://man7.org/linux/man-pages/man2/gettid.2.html
-	struct sched_param sd;
-        cpu_set_t set;
-	sd.sched_priority=50;   //Set real-time priority 50
-	sched_setscheduler(0,SCHED_RR,&sd);
-	CPU_ZERO(&set);
-	CPU_SET(0,&set);
-	sched_setaffinity(0,sizeof(cpu_set_t),&set);   //Set CPU affinity
+  struct sched_param sd;
+  sd.sched_priority = 50; //Set real-time priority 50
+  sched_setscheduler(0, SCHED_RR, & sd);
+  cpu_set_t set;
+  CPU_ZERO( & set);
+  CPU_SET(0, & set);
+
+  if (sched_setaffinity(getpid(), sizeof(set), & set) == -1)
+    printf("\nFailed to set affinity.");
+
+  struct sched_param shparam;
+
+  pthread_attr_t attr[3];
+
+  shparam.sched_priority = 50;
+  pthread_attr_init( & attr[0]);
+  pthread_attr_setschedpolicy( & attr[0], SCHED_FIFO);
+  pthread_attr_setschedparam( & attr[0], & shparam);
+  pthread_attr_setinheritsched( & attr[0], PTHREAD_EXPLICIT_SCHED);
+
+  shparam.sched_priority = 50;
+  pthread_attr_init( & attr[1]);
+  pthread_attr_setschedpolicy( & attr[1], SCHED_FIFO);
+  pthread_attr_setschedparam( & attr[1], & shparam);
+  pthread_attr_setinheritsched( & attr[1], PTHREAD_EXPLICIT_SCHED);
+
+  shparam.sched_priority = 50;
+  pthread_attr_init( & attr[2]);
+  pthread_attr_setschedpolicy( & attr[2], SCHED_FIFO);
+  pthread_attr_setschedparam( & attr[2], & shparam);
+  pthread_attr_setinheritsched( & attr[2], PTHREAD_EXPLICIT_SCHED);
+
   /* ### start section to be measured ### */
   /* gettimeofday() method */
   gettimeofday( & startwatch, NULL);
 
   for (int i = 0; i < n_thread; i++)
-    pthread_create( & tid[i], NULL, runner, (argv[2]));
+    pthread_create( & tid[i], & attr[i], runner, (argv[2]));
+
   /* wait for the thread to exit */
-  for (int i = 0; i < n_thread; i++)
+  for (int i = 0; i < n_thread; i++) {
     pthread_join(tid[i], NULL);
+    pthread_attr_destroy( & attr[i]);
+  }
 
   gettimeofday( & endwatch, NULL);
   /* ### end of section to be measured ### */
@@ -66,9 +97,24 @@ void * runner(void * param) {
   srand((unsigned int) time(NULL));
   float x, y, distance;
   int a = atoi(param);
-
   pid_t tid = syscall(SYS_gettid);
-  printf("\nThread %d is running.", tid);
+
+  struct timeval start, end;
+
+  printf("\nThread %d is running ", tid);
+
+  int policy;
+  struct sched_param shparam;
+  pthread_getschedparam(pthread_self(), & policy, & shparam);
+  if (policy == SCHED_OTHER)
+    printf("SCHED_OTHER\n");
+  if (policy == SCHED_RR)
+    printf("SCHED_RR\n");
+  if (policy == SCHED_FIFO)
+    printf("SCHED_BATCH\n");
+
+  gettimeofday( & start, NULL);
+
   for (int i = 0; i < a; i++) {
     x = -1 + ((float) rand() / (float)(RAND_MAX)) * 2;
     y = -1 + ((float) rand() / (float)(RAND_MAX)) * 2;
@@ -77,165 +123,10 @@ void * runner(void * param) {
     if (distance <= 1.0) counter++;
 
   }
-  printf("\nThread %d finished.", tid);
+
+  gettimeofday( & end, NULL);
+
+  printf("\nThread %d start at %ld.%ld and end at %ld.%ld\n", tid, start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec);
+
   pthread_exit(0);
-}
-
-
-
-
-
-
-
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-/* gettimeofday */
-#include <sys/time.h>
-
-
-
-#include <sched.h>
-void Thread1()
-{ 
-struct timeval start, end;
-    gettimeofday(&start, NULL);
-    int i,j;
-    int policy;
-    struct sched_param param;
-    pthread_getschedparam(pthread_self(),&policy,&param);
-    if(policy == SCHED_OTHER)
-        printf("SCHED_OTHER\n");
-    if(policy == SCHED_RR)
-        printf("SCHED_RR\n");
-    if(policy==SCHED_FIFO)
-         printf("SCHED_FIFO\n");
-    for(i=1;i<10;i++) 
-
-    {
-         for(j=1;j<500000000;j++)
-        {
-        }
-       printf("thread 1\n"); //sleep(1);
-    }
-gettimeofday(&end, NULL);
-   printf("Pthread 1 start at %ld.%ld and end at %ld.%ld\n", start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec);
-}
-
-void Thread2()
-{  
-struct timeval start, end;
-    gettimeofday(&start, NULL);
-    int i,j,m;
-    int policy;
-    struct sched_param param;
-    pthread_getschedparam(pthread_self(),&policy,&param);
-    if(policy == SCHED_OTHER)
-        printf("SCHED_OTHER\n");
-    if(policy == SCHED_RR)
-        printf("SCHED_RR\n");
-    if(policy==SCHED_FIFO)
-        printf("SCHED_FIFO\n");
-sleep(1);
-     for(i=1;i<10;i++) 
-
-    {
-        for(j=1;j<500000000;j++)
-        {
-        }
-       printf("thread 2\n");// sleep(1);
-    }
-
-gettimeofday(&end, NULL);
-
-   printf("Pthread 2 start at %ld.%ld and end at %ld.%ld\n", start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec);
-
-
-
-}
-
-void Thread3()
-{
-struct timeval start, end;
-    gettimeofday(&start, NULL);
-    int i,j;
-    int policy;
-    struct sched_param param;
-    pthread_getschedparam(pthread_self(),&policy,&param);
-    if(policy == SCHED_OTHER)
-        printf("SCHED_OTHER\n");
-    if(policy == SCHED_RR)
-        printf("SCHED_RR\n");
-    if(policy==SCHED_FIFO)
-        printf("SCHED_BATCH\n");
-
-  for(i=1;i<10;i++) 
- 
-     {
-        for(j=1;j<50000000;j++)
-         {
-         }
-
-        printf("thread 3\n"); sleep(1);
-     }
-gettimeofday(&end, NULL);
-
-   printf("Pthread 3 start at %ld.%ld and end at %ld.%ld\n", start.tv_sec, start.tv_usec, end.tv_sec, end.tv_usec);
-}
-
-int main()
-{
-    int i;
-    i = getuid();
-    if(i==0)
-        printf("The current user is root\n");
-    else
-        printf("The current user is not root\n");
-
-	struct sched_param sd;
-        cpu_set_t set;
-	sd.sched_priority=50;   //Set real-time priority 50
-	sched_setscheduler(0,SCHED_RR,&sd);
-	CPU_ZERO(&set);
-	CPU_SET(0,&set);
-	sched_setaffinity(0,sizeof(cpu_set_t),&set);   //Set CPU affinity
-
-
-
-
-    pthread_t ppid1,ppid2,ppid3;
-    struct sched_param param;
-
-    pthread_attr_t attr3,attr1,attr2;
-    pthread_attr_init(&attr1);
-    pthread_attr_init(&attr2);
-    pthread_attr_init(&attr3);
-
-    param.sched_priority = 50;
-    pthread_attr_setschedpolicy(&attr1,SCHED_FIFO);
-    pthread_attr_setschedparam(&attr1,&param);
-    pthread_attr_setinheritsched(&attr1,PTHREAD_EXPLICIT_SCHED);
-
-    param.sched_priority = 50;
-    pthread_attr_setschedpolicy(&attr2,SCHED_FIFO);
-    pthread_attr_setschedparam(&attr2,&param);
-    pthread_attr_setinheritsched(&attr2,PTHREAD_EXPLICIT_SCHED);
-
-    param.sched_priority = 50;
-    pthread_attr_setschedpolicy(&attr3,SCHED_FIFO);
-    pthread_attr_setschedparam(&attr3,&param);
-    pthread_attr_setinheritsched(&attr3,PTHREAD_EXPLICIT_SCHED);
-
-    pthread_create(&ppid3,&attr3,(void *)Thread3,NULL);
-    pthread_create(&ppid2,&attr2,(void *)Thread2,NULL);
-    pthread_create(&ppid1,&attr1,(void *)Thread1,NULL);
-
-    pthread_join(ppid3,NULL);
-    pthread_join(ppid2,NULL);
-    pthread_join(ppid1,NULL);
-    pthread_attr_destroy(&attr2);
-    pthread_attr_destroy(&attr1);
-    return 0;
 }
