bf0661619a50c993f1bb735907fb83b277b293f4
Tran T. Tin <trantin84@gmail.com>
2021-06-10 15:53:36 +0700

    Update task1_1PC.c

--- LAB_7/task1_1PC.c
+++ LAB_7/task1_1PC.c
@@ -1,7 +1,9 @@
 // 2021 June 7
 // Author: Tran Trung Tin
-// Calculating value of PI by Monte Carlo method
-#define _GNU_SOURCE#include<pthread.h>
+// Producer / Consumer
+#define _GNU_SOURCE
+
+#include<pthread.h>
 
 #include<stdio.h>
 
@@ -17,10 +19,17 @@
 
 #include <sys/wait.h>
 
+#include<pthread.h>
+
 // maximum number of threads 
+#define BUFFER_SIZE 10
 #define MAX_THREAD 2
 int count = 0; /* this data is shared by the thread(s) */
-void * producer(void * param); /* threads call this function */
+int in = 0;
+int out = 0;
+int buffer[BUFFER_SIZE];
+void * producer(void * param); /* threads 0 call this function */
+void * consumer(void * param); /* threads 1 call this function */
 int main(int argc, char * argv[]) {
   pthread_t tid[MAX_THREAD]; /* the thread identifier */
 
@@ -30,45 +39,42 @@ int main(int argc, char * argv[]) {
   cpu_set_t set;
   CPU_ZERO( & set);
   CPU_SET(0, & set);
+  CPU_SET(1, & set);
   if (sched_setaffinity(getpid(), sizeof(set), & set) == -1)
     printf("\nFailed to set affinity.");
 
-    pthread_create( & tid[i], & attr[i], producer, (argv[1]));
-    pthread_create( & tid[i], & attr[i], consumer, (argv[1]));
-    pthread_join(tid[i], NULL);
-
-
-printf("\nAfter all, count = %d\n", count);
+  pthread_create( & tid[0], NULL, producer, (argv[1]));
+  pthread_create( & tid[1], NULL, consumer, (argv[1]));
+  pthread_join(tid[0], NULL);
+  pthread_join(tid[1], NULL);
+  
+  printf("\nAfter all, count = %d\n", count);
   return 0;
 }
 
 /* The thread will execute in this function */
-void *producer(void *arg)
-{
- int i=0, max = *((int*) arg);
-for(int i=0; i < max; i++)
- {
- while (count == BUFFER_SIZE);  //do nothing
-  buffer[in] = 1;
- in = (in + 1) % BUFFER_SIZE;
- count++;
- 
- }
+void * producer(void * param) {
+  int i = 0, max = atoi(param);
+  for (int i = 0; i < max; i++) {
+    while (count == BUFFER_SIZE); //do nothing
+    buffer[ in ] = 1;
+    printf("\nJust sent in = %d and count = %d.", in , count);
+    in = ( in +1) % BUFFER_SIZE;
+    count++;
+  }
   pthread_exit(0);
 }
-void *consumer(void *arg)
-{
+void * consumer(void * param) {
   int receive = 0;
- int i=0, max = *((int*) arg);
-for(int i=0; i < max; i++)
- {
- while (count == 0);  //do nothing
- receive += buffer[out];
-out = (out + 1) % BUFFER_SIZE;
- count--;
- 
- }
+  int i = 0, max = atoi(param);
+  for (int i = 0; i < max; i++) {
+    while (count == 0); //do nothing
+    receive += buffer[out];
+    printf("\nJust received out = %d and count = %d.", in , count);
+    out = (out + 1) % BUFFER_SIZE;
+    count--;
+  }
   printf("\nAfter all, receive = %d\n", count);
-  
+
   pthread_exit(0);
 }
