781438e5398fbbd41efb18b56fb431b9d7429e89
Tran T. Tin <trantin84@gmail.com>
2021-06-10 20:55:34 +0700

    Create task5_2.c

---
+++ LAB_7/task5_2.c
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#ifndef __STDC_NO_ATOMICS__#include <stdatomic.h>
+
+#endif
+
+int main(int argc, char ** argv) {
+  _Atomic int a;
+  atomic_init( & a, 42);
+  atomic_store( & a, 5);
+  int b = atomic_load( & a);
+  printf("b = %i\n", b);
+
+  return EXIT_SUCCESS;
+}
