0273b3f310ffefddaf2476ef9b79941cf551c47f
Tran T. Tin <trantin84@gmail.com>
2021-06-11 10:07:46 +0700

    Update Fig3_16_Producer_POSIX_SHM.c

--- CHAPTER_3/Fig3_16_Producer_POSIX_SHM.c
+++ CHAPTER_3/Fig3_16_Producer_POSIX_SHM.c
@@ -20,23 +20,23 @@ int main() {
   /* name of the shared memory object */
   const char * name = "OS";
   /* strings written to shared memory */
-  const char * message 0 = "Hello";
-  const char * message 1 = "World!";
+  const char * message_0 = "Hello";
+  const char * message_1 = "World!";
   /* shared memory file descriptor */
   int fd;
   /* pointer to shared memory obect */
   char * ptr;
   /* create the shared memory object */
-  fd = shm open(name, O CREAT | O RDWR, 0666);
+  fd = shm_open(name, O_CREAT | O_RDWR, 0666);
   /* configure the size of the shared memory object */
   ftruncate(fd, SIZE);
   /* memory map the shared memory object */
   ptr = (char * )
-  mmap(0, SIZE, PROT READ | PROT WRITE, MAP SHARED, fd, 0);
+  mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   /* write to the shared memory object */
-  sprintf(ptr, "%s", message 0);
-  ptr += strlen(message 0);
-  sprintf(ptr, "%s", message 1);
-  ptr += strlen(message 1);
+  sprintf(ptr, "%s", message_0);
+  ptr += strlen(message_0);
+  sprintf(ptr, "%s", message_1);
+  ptr += strlen(message_1);
   return 0;
 }
