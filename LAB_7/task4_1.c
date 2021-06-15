8854447281c639174044e6a2aa96a93d6e8f62c8
Tran T. Tin <trantin84@gmail.com>
2021-06-15 22:21:21 +0700

    Update task4_1.c

--- LAB_7/task4_1.c
+++ LAB_7/task4_1.c
@@ -1,60 +1,37 @@
-void* thread_func() {
-  int i = 0;
-  long long prev = 0;
-  long long sum = 0;
+/* 
+ * Throughput measurement thread. Sleeps for a while then wakes up and measures
+ * the throughput, resetting counters in the process.
+ */
+void
+*measure_thread(void *arg)
+{
+	int c, i;
+	unsigned long long throughput;
+        for (c = 1 ;; c++) {
+                sleep(1);
+                throughput = 0;
+                for (i = 0; i < ncounters; i++) {
+                        if (counters[i]->c) {
 
-  for (i = 0; i < num_iterations; i++) {
-    switch (opt_sync) {
-      case 'c':
-        do {
-          prev = counter;
-          sum = prev + 1;
-          if (opt_yield) {
-            pthread_yield();
-          }
-        } while (__sync_val_compare_and_swap(&counter, prev, sum) != prev);
-        break;
-      case 'm':
-        pthread_mutex_lock(&add_mutex);
-        add(&counter, 1);
-        pthread_mutex_unlock(&add_mutex);
-        break;
-      case 's':
-        while (__sync_lock_test_and_set(&add_spin, 1));
-        add(&counter, 1);
-        __sync_lock_release(&add_spin);
-        break;
-      default:
-        add(&counter, 1);
-        break;
-    }
-  }
+#if defined(SYNC_FETCH_ADD)
+                                throughput += \
+					__sync_lock_test_and_set(&counters[i]->c, 0);
 
-  for (i = 0; i < num_iterations; i++) {
-    switch (opt_sync) {
-      case 'c':
-        do {
-          prev = counter;
-          sum = prev - 1;
-          if (opt_yield) {
-            pthread_yield();
-          }
-        } while (__sync_val_compare_and_swap(&counter, prev, sum) != prev);
-        break;
-      case 'm':
-        pthread_mutex_lock(&add_mutex);
-        add(&counter, -1);
-        pthread_mutex_unlock(&add_mutex);
-        break;
-      case 's':
-        while (__sync_lock_test_and_set(&add_spin, 1));
-        add(&counter, -1);
-        __sync_lock_release(&add_spin);
-        break;
-      default:
-        add(&counter, -1);
-        break;  
-    }
-  }
-  return NULL;
+#elif defined(PTHREAD_SPIN)
+				pthread_spin_lock(&counters[i]->spin);
+				throughput += counters[i]->c;
+				counters[i]->c = 0;
+				pthread_spin_unlock(&counters[i]->spin);
+
+#elif defined(PTHREAD_MUTEX)
+				pthread_mutex_lock(&counters[i]->mutex);
+				throughput += counters[i]->c;
+				counters[i]->c = 0;
+				pthread_mutex_unlock(&counters[i]->mutex);
+#endif
+			}
+		}
+		printf("%lld\n", throughput);
+                fflush(stdout);
+        }
 }
