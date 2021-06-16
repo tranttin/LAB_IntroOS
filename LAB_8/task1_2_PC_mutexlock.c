48d4a01ae626d2ec709fac13b7971aac6a945b99
Tran T. Tin <trantin84@gmail.com>
2021-06-16 22:41:05 +0700

    Update task1_2_PC_mutexlock.c

--- LAB_8/task1_2_PC_mutexlock.c
+++ LAB_8/task1_2_PC_mutexlock.c
@@ -20,14 +20,16 @@
 #include <sys/wait.h>
 
 #include <stdbool.h>
-// maximum number of threads 
+
+/* gettimeofday */ #include <sys/time.h>
+ // maximum number of threads 
 #define BUFFER_SIZE 10
 #define MAX_THREAD 2
 int count = 0; /* this data is shared by the thread(s) */
 int in = 0;
 int out = 0;
 int buffer[BUFFER_SIZE];
-pthread_mutex_t lock; 
+pthread_mutex_t lock;
 void * producer(void * param); /* threads 0 call this function */
 void * consumer(void * param); /* threads 1 call this function */
 int main(int argc, char * argv[]) {
@@ -43,18 +45,22 @@ int main(int argc, char * argv[]) {
   if (sched_setaffinity(getpid(), sizeof(set), & set) == -1)
     printf("\nFailed to set affinity.");
 
-  	if (pthread_mutex_init(&lock, NULL) != 0) 
-	{ 
-		printf("\n mutex init has failed\n"); 
-		return 1; 
-	} 
-  
+  if (pthread_mutex_init( & lock, NULL) != 0) {
+    printf("\n mutex init has failed\n");
+    return 1;
+  }
+  struct timeval startwatch, endwatch;
+  /* gettimeofday() method */
+  gettimeofday( & startwatch, NULL);
   pthread_create( & tid[0], NULL, producer, (argv[1]));
   pthread_create( & tid[1], NULL, consumer, (argv[1]));
   pthread_join(tid[0], NULL);
+  gettimeofday( & endwatch, NULL);
+  /* ### end of section to be measured ### */
+  printf("\nGettimeofday() method: %ldus\n", (endwatch.tv_sec - startwatch.tv_sec) * 1000000 + (endwatch.tv_usec - startwatch.tv_usec));
   sleep(1);
   pthread_cancel(tid[1]);
-  	pthread_mutex_destroy(&lock); 
+  pthread_mutex_destroy( & lock);
   return 0;
 }
 
@@ -66,11 +72,11 @@ void * producer(void * param) {
     buffer[ in ] = 1;
     printf("\nJust sent in = %d and count = %d.", in , count); in = ( in +1) % BUFFER_SIZE;
 
-    pthread_mutex_lock(&lock); 
+    pthread_mutex_lock( & lock);
     /* critical section */
     count++;
     /* end of critical section */
-    pthread_mutex_unlock(&lock); 
+    pthread_mutex_unlock( & lock);
   }
   pthread_exit(0);
 }
@@ -79,12 +85,12 @@ void * consumer(void * param) {
   while (1) {
     while (count == 0); //do nothing
     receive += buffer[out];
-    
-    pthread_mutex_lock(&lock); 
+
+    pthread_mutex_lock( & lock);
     /* critical section */
     count--;
     /* end of critical section */
-    pthread_mutex_unlock(&lock); 
+    pthread_mutex_unlock( & lock);
     printf("\nJust received = %d and count = %d.", receive, count);
     out = (out + 1) % BUFFER_SIZE;
   }
