6f2f219efc9d32fa4cb2e1417217251fdd73c340
Tran T. Tin <trantin84@gmail.com>
2021-06-16 21:32:22 +0700

    Update task4_2.c

--- LAB_7/task4_2.c
+++ LAB_7/task4_2.c
@@ -1,6 +1,6 @@
 // 2021 June 12
-// Author: Tran Trung Tin. Demo in [1] page 266
-// Demo using memory barrier to make a point.
+// Author: Tran Trung Tin. Demo in [1] page 268
+// Demo using compare_and_swap
 #define _GNU_SOURCE
 
 #include<pthread.h>
@@ -17,7 +17,7 @@
 
 void * urgent(void * param); /* threads call this function */
 void * interrupt(void * param); /* threads call this function */
-atomic_int room = 200;
+int owner = 0; //share
 
 int main(int argc, char * argv[]) {
 
@@ -36,23 +36,30 @@ int main(int argc, char * argv[]) {
 }
 /* The thread will execute in this function */
 void * urgent(void * param) {
-if (atomic_compare_exchange_weak(&room, 200, 402) == 0) {
-	  printf("\nA: Urgent in %d sec ...", atoi(param));
-	  sleep(atoi(param)); //try after interval 1 sec
-  }	
-	printf("\nA: I'm done.");
-	atomic_flag_clear( &flag);
+  printf("\n(0-Share  1-Urgent  2-Interrupt)\nRoom for %d.", owner);
+  if (__sync_bool_compare_and_swap( & owner, 0, 1) == true) {
+    printf("\nRoom for %d in %d sec.", owner, atoi(param));
+    sleep(atoi(param)); //try after interval 1 sec
+  }
+
+  if (__sync_bool_compare_and_swap( & owner, 1, 0) == true)
+    printf("\nUrgent leaving room.");
   pthread_exit(0);
 }
 
 /* The thread will execute in this function */
 void * interrupt(void * param) {
   sleep(1); // come late.
-  while (atomic_compare_exchange_weak(&room, 200, 402 == 1) {
-	  printf("\nB: I'm waiting ...");
-	  sleep(1); //try after interval 1 sec
-  }	  
+  while (__sync_bool_compare_and_swap( & owner, 0, 2) == false) {
+
+    printf("\nB: I'm waiting ...");
+
+    sleep(1); //try after interval 1 sec
+  }
   printf("\nB: I'm in ....\n");
-		atomic_flag_clear( &flag);
+  printf("\n(0-Share  1-Urgent  2-Interrupt)\nRoom for %d.", owner);
+  sleep(1);
+  if (__sync_bool_compare_and_swap( & owner, 2, 0) == true) printf("\nInterrupt leaving room.");
+  printf("\n(0-Share  1-Urgent  2-Interrupt)\nRoom for %d.", owner);
   pthread_exit(0);
 }
