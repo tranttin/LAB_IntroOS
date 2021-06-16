86d5876bdec6c3b2c8a0048ea6355a6d11f338c7
Tran T. Tin <trantin84@gmail.com>
2021-06-16 22:42:38 +0700

    Update task1_3_PC_mutexCaution.c

--- LAB_8/task1_3_PC_mutexCaution.c
+++ LAB_8/task1_3_PC_mutexCaution.c
@@ -20,16 +20,16 @@
 #include <sys/wait.h>
 
 #include <stdbool.h>
-  /* gettimeofday */
-#include <sys/time.h>
-// maximum number of threads 
+
+/* gettimeofday */ #include <sys/time.h>
+ // maximum number of threads 
 #define BUFFER_SIZE 10
 #define MAX_THREAD 2
 int count = 0; /* this data is shared by the thread(s) */
 int in = 0;
 int out = 0;
 int buffer[BUFFER_SIZE];
-pthread_mutex_t lock; 
+pthread_mutex_t lock;
 void * producer(void * param); /* threads 0 call this function */
 void * consumer(void * param); /* threads 1 call this function */
 int main(int argc, char * argv[]) {
@@ -45,11 +45,10 @@ int main(int argc, char * argv[]) {
   if (sched_setaffinity(getpid(), sizeof(set), & set) == -1)
     printf("\nFailed to set affinity.");
 
-  	if (pthread_mutex_init(&lock, NULL) != 0) 
-	{ 
-		printf("\n mutex init has failed\n"); 
-		return 1; 
-	} 
+  if (pthread_mutex_init( & lock, NULL) != 0) {
+    printf("\n mutex init has failed\n");
+    return 1;
+  }
   struct timeval startwatch, endwatch;
   /* gettimeofday() method */
   gettimeofday( & startwatch, NULL);
@@ -61,39 +60,44 @@ int main(int argc, char * argv[]) {
   printf("\nGettimeofday() method: %ldus\n", (endwatch.tv_sec - startwatch.tv_sec) * 1000000 + (endwatch.tv_usec - startwatch.tv_usec));
   sleep(1);
   pthread_cancel(tid[1]);
-  pthread_mutex_destroy(&lock); 
+  pthread_mutex_destroy( & lock);
   return 0;
 }
 
 /* The thread will execute in this function */
 void * producer(void * param) {
+
   int i = 0, max = atoi(param);
   for (int i = 0; i < max; i++) {
+
     while (count == BUFFER_SIZE); //do nothing
+    pthread_mutex_lock( & lock);
     buffer[ in ] = 1;
     printf("\nJust sent in = %d and count = %d.", in , count); in = ( in +1) % BUFFER_SIZE;
 
-    pthread_mutex_lock(&lock); 
     /* critical section */
     count++;
     /* end of critical section */
-    pthread_mutex_unlock(&lock); 
+    pthread_mutex_unlock( & lock);
   }
+
   pthread_exit(0);
 }
 void * consumer(void * param) {
   int receive = 0;
   while (1) {
+
     while (count == 0); //do nothing
+    pthread_mutex_lock( & lock);
     receive += buffer[out];
-    
-    pthread_mutex_lock(&lock); 
+
     /* critical section */
     count--;
     /* end of critical section */
-    pthread_mutex_unlock(&lock); 
+
     printf("\nJust received = %d and count = %d.", receive, count);
     out = (out + 1) % BUFFER_SIZE;
+    pthread_mutex_unlock( & lock);
   }
   pthread_exit(0);
 }
