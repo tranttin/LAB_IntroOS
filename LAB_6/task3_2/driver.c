69116f8bd5d1bbe977c5ddf2e03bdbb1d5ecb5b4
Tran T. Tin <trantin84@gmail.com>
2021-06-18 21:11:51 +0700

    Update driver.c

--- LAB_6/task3_2/driver.c
+++ LAB_6/task3_2/driver.c
@@ -1,56 +1,51 @@
 // 2021 June 7
 // Author: Tran Trung Tin
-// Calculating value of PI by Monte Carlo method
+// Reading file and import to array to run scheduling algorithm
 
 #include "task.h"
  // maximum number of threads 
 #define MAX_THREAD 5
 
-int i = 0;
+int process = 0;
 
 void * reader(void * param);
 int main(int argc, char * argv[]) {
+  void * status;
   pthread_t tid[MAX_THREAD]; /* the thread identifier */
-
   struct timeval startwatch, endwatch;
 
   /* ### start section to be measured ### */
-  /* gettimeofday() method */
   gettimeofday( & startwatch, NULL);
-
   pthread_create( & tid[0], NULL, reader, (argv[1]));
-  pthread_join(tid[0], NULL);
+  pthread_join(tid[0], & status);
 
+  if ((intptr_t) status == -1) return -1; //cannot open file
   pthread_create( & tid[0], NULL, FCFS, NULL);
   pthread_join(tid[0], NULL);
 
   gettimeofday( & endwatch, NULL);
   /* ### end of section to be measured ### */
 
-  printf("\nGettimeofday() method: %ldus\n", (endwatch.tv_sec - startwatch.tv_sec) * 1000000 + (endwatch.tv_usec - startwatch.tv_usec));
-
+  printf("\nRunningd: %ldus\n", (endwatch.tv_sec - startwatch.tv_sec) * 1000000 + (endwatch.tv_usec - startwatch.tv_usec));
   return 0;
 }
 
 /* The thread will execute in this function */
 void * reader(void * param) {
-
   FILE * fp;
   if ((fp = fopen(param, "r")) == NULL) {
-    printf("\nCannot open file.");
+    printf("\nCannot open file.\n");
     return (void * ) - 1;
   }
-
   char * temp;
   char job[SIZE];
-
   while (fgets(job, SIZE, fp) != NULL) {
     temp = strdup(job);
-    task[i].name = strsep( & temp, ",");
-    task[i].priority = atoi(strsep( & temp, ","));
-    task[i].arrival = atoi(strsep( & temp, ","));
-    task[i].burst = atoi(strsep( & temp, ","));
-    i++;
+    task[process].name = strsep( & temp, ",");
+    task[process].priority = atoi(strsep( & temp, ","));
+    task[process].arrival = atoi(strsep( & temp, ","));
+    task[process].burst = atoi(strsep( & temp, ","));
+    process++;
     free(temp);
   }
   fclose(fp);
@@ -59,4 +54,12 @@ void * reader(void * param) {
 
 void run(Task * task, int start, int slice) {
   printf("Running task = [%s] [%d] [%d] [%d] from %d to %d.\n", task -> name, task -> priority, task -> arrival, task -> burst, start, slice + start);
+}
+
+void swap(Task * a, Task * b) {
+  Task c;
+  c = * a;
+  * a = * b;
+  * b = c;
+  return;
 }
