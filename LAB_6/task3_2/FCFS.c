2569b6b7a9abd8870fb714f71fcd5f9298aa997a
Tran T. Tin <trantin84@gmail.com>
2021-06-19 07:46:41 +0700

    Update FCFS.c

--- LAB_6/task3_2/FCFS.c
+++ LAB_6/task3_2/FCFS.c
@@ -3,19 +3,19 @@
 // FCFS running an array already read from file by driver.c
 
 #include "task.h"
+
 extern int process;
 
-void * FCFS(void * param){
-    int t_wait = 0, t_taround = 0;
-    int j=0;
-    int time=0;
-//SORTING ARRIVAL TIME
-    for(j=0 ; j < process ; j++) {
-        run(&task[j],time,task[j].burst); //Run from time to time+burst
-        time += task[j].burst; //moving to next P
-        t_wait += time - task[j].burst - task[j].arrival ;
-        t_taround += time - task[j].arrival ;
-    }
+void * FCFS(void * param) {
+  int t_wait = 0, t_taround = 0;
+  int time = 0;
+  //SORTING ARRIVAL TIME
+  for (int i = 0; i < process; i++) {
+    run( & task[i], time, task[i].burst); //Run from time to time+burst
+    time += task[i].burst; //moving to next P
+    t_wait += time - task[i].burst - task[i].arrival;
+    t_taround += time - task[i].arrival;
+  }
   printf("\nFCFS Time wait ave = %.2f", t_wait * 1.0 / process);
   printf("\nFCFS Time turn around ave = %.2f", t_taround * 1.0 / process);
   pthread_exit(0);
