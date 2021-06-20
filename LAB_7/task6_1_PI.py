ace83636c7b2adba0f8b9216b55c715fc52e50f7
Tran T. Tin <trantin84@gmail.com>
2021-06-20 17:14:14 +0700

    Create task6_1_PI.py

---
+++ LAB_7/task6_1_PI.py
@@ -0,0 +1,35 @@
+import random
+import math
+import threading
+counter = 0
+seed = 1000000
+
+def thread_task(lock):
+  global counter
+  for i in range(seed):
+    x=random.random()
+    y=random.random()
+    dist = math.sqrt (x*x + y * y)
+    if (dist < 1.0): 
+      lock.acquire()
+      counter=counter+1;
+      lock.release()
+
+def main():
+  # creating a lock
+  lock = threading.Lock()
+  
+  t1 = threading.Thread(target=thread_task, args=(lock,))
+  t2 = threading.Thread(target=thread_task, args=(lock,))
+
+  # start threads
+  t1.start()
+  t2.start()
+
+  # wait until threads finish their job
+  t1.join()
+  t2.join()
+  PI = counter/(seed*2)*4
+  print(PI)
+
+main()
