38a5544ccb6c6304bad1dc71f01c0d293e19dc98
Tran T. Tin <trantin84@gmail.com>
2021-06-21 09:42:04 +0700

    Update task1_1Mutexlock.c

--- LAB_8/task1_1Mutexlock.c
+++ LAB_8/task1_1Mutexlock.c
@@ -1,52 +1,46 @@
-#include<stdio.h> 
-#include<string.h> 
-#include<pthread.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
+//2021 March 15
+//Author: G4G
+//Demo of mutex loc
+#include<stdio.h>
 
-pthread_t tid[2]; 
-int counter; 
-pthread_mutex_t lock; 
+#include<string.h>
 
-void* trythis(void *arg) 
-{ 
-	pthread_mutex_lock(&lock); 
+#include<pthread.h>
 
-	unsigned long i = 0; 
-	counter += 1; 
-	printf("\n Job %d has started\n", counter); 
+#include<stdlib.h>
 
-	for(i=0; i<(0xFFFFFFFF);i++); 
+#include<unistd.h>
 
-	printf("\n Job %d has finished\n", counter); 
+pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
 
-	pthread_mutex_unlock(&lock); 
+void * trythis(void * arg) {
+  pthread_mutex_lock( & lock);
+  unsigned long i = 0;
+  counter += 1;
+  printf("\n Job %d has started\n", counter);
+  for (i = 0; i < (0xFFFFFFFF); i++);
+  printf("\n Job %d has finished\n", counter);
+  pthread_mutex_unlock( & lock);
+  return NULL;
+}
 
-	return NULL; 
-} 
-
-int main(void) 
-{ 
-	int i = 0; 
-	int error; 
-
-	if (pthread_mutex_init(&lock, NULL) != 0) 
-	{ 
-		printf("\n mutex init has failed\n"); 
-		return 1; 
-	} 
-
-	while(i < 2) 
-	{ 
-		error = pthread_create(&(tid[i]), NULL, &trythis, NULL); 
-		if (error != 0) 
-			printf("\nThread can't be created :[%s]", strerror(error)); 
-		i++; 
-	} 
-
-	pthread_join(tid[0], NULL); 
-	pthread_join(tid[1], NULL); 
-	pthread_mutex_destroy(&lock); 
-
-	return 0; 
-} 
+int main(void) {
+  int i = 0;
+  int error;
+  if (pthread_mutex_init( & lock, NULL) != 0) {
+    printf("\n mutex init has failed\n");
+    return 1;
+  }
+  while (i < 2) {
+    error = pthread_create( & (tid[i]), NULL, & trythis, NULL);
+    if (error != 0)
+      printf("\nThread can't be created :[%s]", strerror(error));
+    i++;
+  }
+  pthread_join(tid[0], NULL);
+  pthread_join(tid[1], NULL);
+  pthread_mutex_destroy( & lock);
+  return 0;
+}
