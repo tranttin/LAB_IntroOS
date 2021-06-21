cf4c63a1ad4353db7ae4453631f3e782f9a7b31a
Tran T. Tin <trantin84@gmail.com>
2021-06-21 08:32:02 +0700

    Update task4_1_Room.c

--- LAB_7/task4_1_Room.c
+++ LAB_7/task4_1_Room.c
@@ -13,14 +13,11 @@
 
 #include <stdbool.h>
 
-#include <stdatomic.h>
-
 void * urgent(void * param); /* threads call this function */
 void * interrupt(void * param); /* threads call this function */
-atomic_bool flag = false;
+bool flag = false;
 
 int main(int argc, char * argv[]) {
-
   pthread_t tid[2]; /* the thread identifier */
   pthread_attr_t attr; /* set of thread attributes */
   /* set the default attributes of the thread */
@@ -31,28 +28,27 @@ int main(int argc, char * argv[]) {
   /* wait for the thread to exit */
   for (int i = 0; i < 2; i++)
     pthread_join(tid[i], NULL);
-
   return 0;
 }
 /* The thread will execute in this function */
 void * urgent(void * param) {
-if (atomic_flag_test_and_set(&flag) == 0) {
+if (__atomic_test_and_set (&flag, __ATOMIC_RELAXED) == 0) {
 	  printf("\nA: Urgent in %d sec ...", atoi(param));
 	  sleep(atoi(param)); //try after interval 1 sec
   }	
 	printf("\nA: I'm done.");
-	atomic_flag_clear( &flag);
+	__atomic_clear (&flag, __ATOMIC_RELEASE);
   pthread_exit(0);
 }
 
 /* The thread will execute in this function */
 void * interrupt(void * param) {
   sleep(1); // come late.
-  while (atomic_flag_test_and_set(&flag) == 1) {
+  while (__atomic_test_and_set (&flag, __ATOMIC_RELEASE) == 1) {
 	  printf("\nB: I'm waiting ...");
 	  sleep(1); //try after interval 1 sec
   }	  
   printf("\nB: I'm in ....\n");
-		atomic_flag_clear( &flag);
+		__atomic_clear (&flag, __ATOMIC_RELEASE);
   pthread_exit(0);
 }
