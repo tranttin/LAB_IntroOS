a75cb38ca8c02a9fa7d31a5d2781fef4b02b5cc9
Tran T. Tin <trantin84@gmail.com>
2021-06-22 20:03:35 +0700

    Update task5_1_openMP.c

--- LAB_9/task5_1_openMP.c
+++ LAB_9/task5_1_openMP.c
@@ -15,12 +15,11 @@ int main(int argc, char * argv[]) {
     // Getting thread number
     tid = omp_get_thread_num();
     printf("Welcome to GFG from thread = %d\n", tid);
-
     if (tid == 0) {
-
       // Only master thread does this
       nthreads = omp_get_num_threads();
-      printf("Number of threads = %d\n", nthreads);
+      printf("Number of threads = %d\n",
+        nthreads);
     }
   }
 }
