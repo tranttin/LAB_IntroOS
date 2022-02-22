d07653ecbfdf8cad41fbe0e587d7e6724a6fe5f2
Tin T. Tran <trantin84@gmail.com>
2022-02-22 11:58:09 +0100

    Create Ex1.c

---
+++ CHAPTER_3/Ex1.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+int value = 5;
+int main()
+{
+pid t pid;
+pid = fork();
+if (pid == 0) { /* child process */
+value += 15;
+return 0;
+  }
+else if (pid > 0) { /* parent process */
+wait(NULL);
+printf("PARENT: value = %d",value); /* LINE A */
+return 0;
+}
+}
