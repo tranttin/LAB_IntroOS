b312a72aaf7cc6fbb9603533aeaf42c34635e5e8
Tin T. Tran <trantin84@gmail.com>
2022-02-25 08:36:42 +0100

    Update task2_1.c

--- LAB_2/task2_1.c
+++ LAB_2/task2_1.c
@@ -7,6 +7,6 @@ int main(int argc, char ** argv) {
     printf("Number of arguments %i n", argc);
     int i = 0;
     for (i = 0; i < argc; i++)
-      prinf(“Argument % s\ t”, argv[i]);
+      printf("Argument %s\t", argv[i]);
     return 0;
 }
