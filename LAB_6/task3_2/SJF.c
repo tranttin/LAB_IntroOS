d73353574fe1f2934172667498501940f4d0b4b8
Tin T. Tran <trantin84@gmail.com>
2022-03-26 08:32:58 +0700

    Create SJF.c

---
+++ LAB_6/task3_2/SJF.c
@@ -0,0 +1,22 @@
+// 2021 Jun 15
+// Author: Tran Trung Tin, based on [1] Chapter 5 Programming Assignment.
+// FCFS running an array already read from file by driver.c
+
+#include "task.h"
+
+extern int process;
+
+void * FCFS(void * param) {
+  int t_wait = 0, t_taround = 0;
+  int time = 0;
+  //SORTING ARRIVAL TIME
+  for (int i = 0; i < process; i++) {
+    run( & task[i], time, task[i].burst); //Run from time to time+burst
+    time += task[i].burst; //moving to next P
+    t_wait += time - task[i].burst - task[i].arrival;
+    t_taround += time - task[i].arrival;
+  }
+  printf("\nFCFS Time wait ave = %.2f", t_wait * 1.0 / process);
+  printf("\nFCFS Time turn around ave = %.2f", t_taround * 1.0 / process);
+  pthread_exit(0);
+}
