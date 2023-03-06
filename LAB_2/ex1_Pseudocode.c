585550fcd96c7d49daee1fccd471833885ddd008
Tin T. Tran <trantin84@gmail.com>
2023-03-06 13:49:23 +0700

    Create ex1_Pseudocode.c

---
+++ LAB_2/ex1_Pseudocode.c
@@ -0,0 +1,21 @@
+// 2023 Mar 6
+// Author: Tran Trung Tin
+// Viết chương trình sau cho khi truyền đối số n vào thì xuất ra tổng S = 1 + 2 + .... + n
+// a. Báo lỗi nếu lời gọi có đối số không phải là một số nguyên dương.
+// b. Báo lỗi nếu có nhiều hơn 2 đối số (là main.out và n).
+
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+  if(argc != ???? ) 
+  {
+    printf("Number of arguments wrong (case b.)");
+    return -1;
+  }
+  
+  else
+    int n = atoi(argv[n]);
+  printf("\nn= %d", n);
+  
+  return 0;
+}
