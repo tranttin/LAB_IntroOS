5665437df0113e33e97bee86e0611d4b41a0b688
Tin T. Tran <trantin84@gmail.com>
2023-03-07 10:22:28 +0700

    Create BT4_loop.c

---
+++ LAB_2/BT4_loop.c
@@ -0,0 +1,12 @@
+// Infinite loop
+// Open System Monitor to view
+// Ctrl + Z to kill process
+#include <stdio.h>
+
+int main() {
+  printf("Open SYSTEM MONITOR to view this process.\n");
+  printf("Press Ctrl + C to kill this process.\n");
+  printf("Press Ctrl + Z to pause this process.\n");
+  while(1);
+  return 0;
+}
