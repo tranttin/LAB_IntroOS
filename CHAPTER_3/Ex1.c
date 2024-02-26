03f02b1f297f08c7141999708f41442bc9ecb503
Tin T. Tran <trantin84@gmail.com>
2024-02-26 10:53:40 +0700

    Update Ex1.c

--- CHAPTER_3/Ex1.c
+++ CHAPTER_3/Ex1.c
@@ -1,18 +1,20 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/wait.h>
 int value = 5;
+
 int main()
 {
-pid_t pid;
-pid = fork();
-if (pid == 0) { /* child process */
-value += 15;
-return 0;
-  }
-else if (pid > 0) { /* parent process */
-wait(NULL);
-printf("PARENT: value = %d",value); /* LINE A */
-return 0;
-}
+  pid_t pid;
+  pid = fork();
+  if (pid == 0) { /* child process */
+    value += 15;
+    return 0;
+    }
+  else if (pid > 0) { /* parent process */
+    wait(NULL);
+    printf("PARENT: value = %d",value); /* LINE A */
+    return 0;
+   }
 }
